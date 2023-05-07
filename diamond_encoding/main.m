5216cf2d33d15ed013c5d44d416a6bae32000b0d
17moonup <17moonup@gmail.com>
2023-05-07 21:34:47 +0800

    update_lsb_dc_emb

--- diamond_encoding/main.m
+++ diamond_encoding/main.m
@@ -1,21 +1,46 @@
-clc ;clear ;close all;
-text = 'This is a secret message.'; % 要隱藏的文字
-binary_text = reshape(dec2bin(text, 8).'-'0', 1, [])'; % 將文字轉換為二元串
-img = imread('stego.bmp'); % 讀取圖像
-gray_img = rgb2gray(img); % 將圖像轉換為灰度圖像
-compressed_text = dc(binary_text); % 使用鑽石編碼壓縮二元串
-compressed_text = double(compressed_text);
-compressed_text_len = length(compressed_text); % 壓縮後的二元串的長度
-img_size = size(gray_img); % 圖像的大小
-img_len = img_size(1) * img_size(2); % 圖像的像素數量
-if compressed_text_len > img_len % 確保隱藏的資料不超過圖像的大小
-    error('Data too large to embed in image.');
+img = imread('stego.bmp');
+
+
+[m, n, ~] = size(img);
+
+
+secret_info = 'Hello World!';
+
+
+binary_secret = dec2bin(double(secret_info), 8);
+
+
+num_bits = length(binary_secret);
+
+
+if m*n*3 < num_bits
+    error('?片太小,?法容???量的信息!');
 end
-gray_img_vec = reshape(gray_img, [], 1); % 將灰度圖像轉換為向量
-for i = 1:compressed_text_len % 將壓縮後的二元串嵌入到圖像中
-    gray_img_vec(i) = bitset(gray_img_vec(i), 1, compressed_text(i));
+
+diff_mat = randi([0 1], m, n);
+
+for i = 1:num_bits
+    
+    bit = binary_secret(i);
+
+    if bit == '1'     
+        [x1, x2] = find(diff_mat == 0);
+        diff_mat(x1, x2) = 1;
+        diff_mat(x2, x1) = 1;
+    end
 end
-embedded_img = reshape(gray_img_vec, img_size); % 將嵌入資料後的向量轉換為圖像
-imwrite(embedded_img, 'embedde5d_lena.bmp');
-gray_img_vec = num2str(gray_img_vec);
-dc_de(gray_img_vec);
+
+
+stego_img = img;
+for i = 1:m
+    for j = 1:n
+        if diff_mat(i, j) == 1
+            stego_img(i, j, 1) = bitxor(stego_img(i, j, 1), 1);  
+            stego_img(i, j, 2) = bitxor(stego_img(i, j, 2), 1);  
+        end
+    end
+end
+
+
+figure, subplot(1,2,1), imshow(img), title('');
+subplot(1,2,2), imshow(stego_img), title('');
