effb15e12f4cfa510b859e5f459f8595408f4b1f
17moonup <17moonup@gmail.com>
2023-05-18 13:59:21 +0800

    gpc

--- steganalysis/gpc/main.m
+++ steganalysis/gpc/main.m
@@ -1,5 +1,5 @@
 clc;clear;close all;
-stegoimg = imread('stegoimgrando.bmp');
+stegoimg = imread('stegoimg0.9.bmp');
 [m, n, numChannles] = size(stegoimg);
 if numChannles == 1
     gpc(stegoimg,m,n);
@@ -7,6 +7,13 @@ else
     stegoimg = rgb2gray(stegoimg);
     gpc(stegoimg,m,n);
 end
-x = [1.0389,1.0077,0.9897];
-y = [0,0.5,1];
-plot(y,x);
+
+x_train = [0, 0.1,0.2,0.4,0.5,0.8,0.9,1];
+y_train = [1, 1.0027, 1.0137, 1.0487, 1.4375,1.5837,1.9975, 3.0118];
+
+model = fitlm(y_train,x_train);
+y_pred = ans;
+x_pred = predict(model, y_pred);
+
+disp(model);
+disp(x_pred);
