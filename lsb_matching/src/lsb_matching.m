910f737c758eb0a495aef221c37283cb71ca4fe8
17moonup <17moonup@gmail.com>
2023-05-22 18:45:52 +0800

    lsb_matching_rando

--- lsb_matching/src/lsb_matching.m
+++ lsb_matching/src/lsb_matching.m
@@ -1,13 +1,13 @@
-function [stegoimg] = lsb_full_en(simg,stego)
+function [stego] = lsb_matching(simg,cover)
     
-    [M,N] = size(stego);
+    [M,N] = size(cover);
     [m,n] = size(simg);
   
     %evaluate
     
     if m>M || n>N
         simg = imresize(simg,[M,N]);
-        lsb_full_en(simg, stego);
+        lsb_matching(simg, cover);
     end
     
     %lsb_encode
@@ -16,27 +16,27 @@ function [stegoimg] = lsb_full_en(simg,stego)
     if(n<=N && m<=M)
         for i = 1:m
             for j = 1:n
-                if simg(i,j) == bitget(stego(i,j),1)
+                if simg(i,j) == bitget(cover(i,j),1)
                     continue;
-                elseif simg(i,j) == 0 && bitget(stego(i,j),1) == 1 && stego(i,j) ~= 255
+                elseif simg(i,j) == 0 && bitget(cover(i,j),1) == 1 && cover(i,j) ~= 255
                    
                     %if odd min 1
                     
                     tmp = randi([0, 1], 1)*2 - 1;
-                    stego(i,j) = stego(i,j) + tmp;
+                    cover(i,j) = cover(i,j) + tmp;
                     rando(i,j) = tmp;
-                elseif simg(i,j) == 1 && bitget(stego(i,j),1) == 0 && stego(i,j) ~= 0
+                elseif simg(i,j) == 1 && bitget(cover(i,j),1) == 0 && cover(i,j) ~= 0
                    
                     %if even plus 1
                     
                     tmp = randi([0, 1], 1)*2 - 1;
                     
-                    stego(i,j) = stego(i,j) + tmp;
+                    cover(i,j) = cover(i,j) + tmp;
                     rando(i,j) = tmp;
-                elseif stego(i,j) == 255
-                    stego(i,j) = stego(i,j) - 1;
-                elseif stego(i,j) == 0
-                    stego(i,j) = stego(i,j) + 1;
+                elseif cover(i,j) == 255
+                    cover(i,j) = cover(i,j) - 1;
+                elseif cover(i,j) == 0
+                    cover(i,j) = cover(i,j) + 1;
                 end
             end
         end
@@ -44,7 +44,7 @@ function [stegoimg] = lsb_full_en(simg,stego)
         fprintf('Secret img is too big for stego!!!');
     end
     
-stegoimg = stego;
-imwrite(stegoimg, 'stegoimg.bmp');
+stego = cover;
+imwrite(stego, 'stego.bmp');
 
 end
---
+++ lsb_rando_matching/src/randomlsb.m
@@ -0,0 +1,40 @@
+function [stego] = randomlsb(simg,cover)
+    
+    [m,n] = size(simg);
+    [M,N] = size(cover);
+    
+    if M >= m && N >= n 
+        R = randi(255,m,n);
+        [T1,T2] = sort(R,'ascend');
+        display(T1);
+        for i = 1:m
+            for j = 1:n
+                if simg(i,j) == bitget(cover(T2(i),T2(j)),1)
+                    continue;
+                elseif simg(i,j) == 0 && bitget(cover(T2(i),T2(j)),1) == 1 && cover(T2(i),T2(j)) ~= 255
+                   
+                    %if odd min 1
+                    
+                    tmp = randi([0, 1], 1)*2 - 1;
+                    cover(T2(i),T2(j)) = cover(T2(i),T2(j)) + tmp;
+                    
+                elseif simg(i,j) == 1 && bitget(cover(T2(i),T2(j)),1) == 0 && cover(T2(i),T2(j)) ~= 0
+                   
+                    %if even plus 1
+                    
+                    tmp = randi([0, 1], 1)*2 - 1; 
+                    cover(T2(i),T2(j)) = cover(T2(i),T2(j) + tmp;
+             
+                elseif cover(T2(i),T2(j)) == 255
+                    cover(T2(i),T2(j)) = cover(T2(i),T2(j)) - 1;
+                elseif cover(T2(i),T2(j)) == 0
+                    cover(T2(i),T2(j)) = cover(T2(i),T2(j)) + 1;
+                end
+            end
+        end
+       
+    end
+    stego = cover;
+    imwrite(stego,'stegoimgrando.bmp');
+    dlmwrite('randomatrix.txt' , R);
+end
